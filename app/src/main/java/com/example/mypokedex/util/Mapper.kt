444ea878cf197f7b1da69ea5cb8689e6e053dc52
lumilitas <lucas23121995>
2020-07-28 09:03:32 -0300

    Create mapper to convert objects

--- app/src/main/java/com/example/mypokedex/util/Mapper.kt
+++ app/src/main/java/com/example/mypokedex/util/Mapper.kt
@@ -4,26 +4,24 @@ import com.example.mypokedex.model.dto.PokemonDto
 import com.example.mypokedex.model.entity.PokemonEntity
 import com.example.mypokedex.model.ui.Pokemon
 
-class Mapper {
 
-    fun PokemonDto.toPokemon(): Pokemon {
-        return Pokemon(
-            id = this.id,
-            name = this.name,
-            height = this.height,
-            weight = this.weight,
-            sprite = this.sprites.frontDefault
-        )
-    }
 
-    fun PokemonEntity.toPokemon(): Pokemon {
-        return Pokemon(
-            id = this.id,
-            name = this.name,
-            height = this.height,
-            weight = this.weight,
-            sprite = this.sprite
-        )
-    }
+fun PokemonDto.toPokemon(): Pokemon {
+    return Pokemon(
+        id = this.id,
+        name = this.name,
+        height = this.height,
+        weight = this.weight,
+        sprite = this.sprites.frontDefault
+    )
+}
 
+fun PokemonEntity.toPokemon(): Pokemon {
+    return Pokemon(
+        id = this.id,
+        name = this.name,
+        height = this.height,
+        weight = this.weight,
+        sprite = this.sprite
+    )
 }
